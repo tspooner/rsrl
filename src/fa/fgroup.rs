6dfc3ed06af4e58d337fecd7d9808141ebd35c5b
Tom Spooner <spooner10000@gmail.com>
2017-05-07 14:55:26 +0100

    Added tests for VFunctionGroup

--- src/fa/fgroup.rs
+++ src/fa/fgroup.rs
@@ -41,3 +41,69 @@ impl<S: Space, V: VFunction<S>> QFunction<S> for VFunctionGroup<S, V>
         self.0[action].update(state, error);
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::{VFunctionGroup, Function, Parameterised, VFunction, QFunction};
+    use geometry::RegularSpace;
+    use geometry::dimensions::Continuous;
+
+
+    pub struct Mock(f64);
+
+    impl Function<Vec<f64>, f64> for Mock {
+        fn evaluate(&self, _: &Vec<f64>) -> f64 { self.0 }
+    }
+
+    impl Parameterised<Vec<f64>, f64> for Mock {
+        fn update(&mut self, _: &Vec<f64>, e: f64) { self.0 = e; }
+    }
+
+    impl VFunction<RegularSpace<Continuous>> for Mock {}
+
+
+    #[test]
+    fn test_function() {
+        let fg = VFunctionGroup::new(vec![Mock(0.0), Mock(1.0), Mock(2.0)]);
+
+        assert_eq!(fg.evaluate(&vec![]), vec![0.0, 1.0, 2.0]);
+    }
+
+    #[test]
+    fn test_parameterised() {
+        let mut fg = VFunctionGroup::new(vec![Mock(0.0), Mock(1.0), Mock(2.0)]);
+
+        fg.update(&vec![], vec![4.0, -1.0, 8.0]);
+
+        assert_eq!(fg.evaluate(&vec![]), vec![4.0, -1.0, 8.0]);
+    }
+
+    #[test]
+    fn test_qfunction() {
+        let fg = VFunctionGroup::new(vec![Mock(0.0), Mock(1.0), Mock(2.0)]);
+
+        assert_eq!(fg.evaluate_action(&vec![], 0), 0.0);
+        assert_eq!(fg.evaluate_action(&vec![], 1), 1.0);
+        assert_eq!(fg.evaluate_action(&vec![], 2), 2.0);
+    }
+
+    #[test]
+    fn test_boxed_vfuncs() {
+        let mut fg = VFunctionGroup::new(vec![
+            Box::new(Mock(0.0)),
+            Box::new(Mock(1.0)),
+            Box::new(Mock(2.0))
+        ]);
+
+        assert_eq!(fg.evaluate(&vec![]), vec![0.0, 1.0, 2.0]);
+
+        assert_eq!(fg.evaluate_action(&vec![], 0), 0.0);
+        assert_eq!(fg.evaluate_action(&vec![], 1), 1.0);
+        assert_eq!(fg.evaluate_action(&vec![], 2), 2.0);
+
+        fg.update(&vec![], vec![4.0, -1.0, 8.0]);
+
+        assert_eq!(fg.evaluate(&vec![]), vec![4.0, -1.0, 8.0]);
+    }
+}
