47c599b157e93d0a5c81acb11f0d51f4014ca106
Tom Spooner <spooner10000@gmail.com>
2017-05-10 13:01:07 +0100

    Added tests for Span enum

--- src/geometry/span.rs
+++ src/geometry/span.rs
@@ -1,7 +1,7 @@
 use std::ops::Mul;
 
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Span {
     Null,
     Finite(usize),
@@ -31,6 +31,73 @@ impl Into<usize> for Span {
         match self {
             Span::Finite(e) => e,
             _ => panic!("Span type has no integer representation.")
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::Span;
+
+    #[test]
+    fn test_equality() {
+        assert_eq!(Span::Null, Span::Null);
+        assert_eq!(Span::Infinite, Span::Infinite);
+
+        assert_eq!(Span::Finite(1), Span::Finite(1));
+        assert_eq!(Span::Finite(5), Span::Finite(5));
+        assert_eq!(Span::Finite(10), Span::Finite(10));
+    }
+
+    #[test]
+    fn test_inequality() {
+        assert_ne!(Span::Null, Span::Infinite);
+        assert_ne!(Span::Null, Span::Finite(1));
+
+        assert_ne!(Span::Infinite, Span::Null);
+        assert_ne!(Span::Infinite, Span::Finite(1));
+
+        assert_ne!(Span::Finite(1), Span::Null);
+        assert_ne!(Span::Finite(1), Span::Infinite);
+        assert_ne!(Span::Finite(1), Span::Finite(10));
+    }
+
+    #[test]
+    fn test_mul() {
+        assert_eq!(Span::Null * Span::Null, Span::Null);
+        assert_eq!(Span::Infinite * Span::Infinite, Span::Infinite);
+
+        assert_eq!(Span::Null * Span::Infinite, Span::Infinite);
+        assert_eq!(Span::Infinite * Span::Null, Span::Infinite);
+
+        assert_eq!(Span::Finite(1) * Span::Finite(1), Span::Finite(1));
+        assert_eq!(Span::Finite(1) * Span::Finite(5), Span::Finite(5));
+        assert_eq!(Span::Finite(5) * Span::Finite(1), Span::Finite(5));
+        assert_eq!(Span::Finite(5) * Span::Finite(5), Span::Finite(25));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_into_null() {
+        let s = Span::Null;
+        let v: usize = s.into();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_into_infinite() {
+        let s = Span::Infinite;
+        let v: usize = s.into();
+    }
+
+    #[test]
+    fn test_into_finite() {
+        for i in vec![1, 5, 10] {
+            let d = Span::Finite(i);
+            let v: usize = d.into();
+
+            assert_eq!(v, i);
         }
     }
 }
