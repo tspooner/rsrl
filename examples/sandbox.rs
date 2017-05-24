220fcf0db100c858ad189d565bf7c0fb4828cc80
Tom Spooner <spooner10000@gmail.com>
2017-05-24 16:58:13 +0100

    Change example slightly

--- examples/sandbox.rs
+++ examples/sandbox.rs
@@ -1,9 +1,9 @@
 extern crate rsrl;
 
 use rsrl::{run, SerialExperiment, Evaluation};
-use rsrl::fa::linear::UniformGrid;
+use rsrl::fa::linear::RBFNetwork;
 use rsrl::agents::control::td::QLearning;
-use rsrl::domains::{Domain, CartPole};
+use rsrl::domains::{Domain, MountainCar};
 use rsrl::policies::{Greedy, EpsilonGreedy};
 use rsrl::geometry::Space;
 
@@ -11,29 +11,29 @@ use rsrl::logging::DefaultLogger;
 
 
 fn main() {
-    let domain = CartPole::default();
+    let domain = MountainCar::default();
     let mut agent = {
         let aspace = domain.action_space();
         let n_actions: usize = aspace.span().into();
 
-        let q_func = UniformGrid::new(
-            domain.state_space().partitioned(10), n_actions);
+        let q_func = RBFNetwork::new(
+            domain.state_space().partitioned(8), n_actions);
 
-        QLearning::new(q_func, Greedy, 0.2, 0.95)
+        QLearning::new(q_func, Greedy, 0.1, 0.95)
     };
 
     // Training:
     let training_result = {
         let e = SerialExperiment::new(&mut agent,
-                                      Box::new(CartPole::default),
+                                      Box::new(MountainCar::default),
                                       1000);
 
-        run(e, 2000)
+        run(e, 1000)
     };
 
     // Testing:
     let testing_result =
-        Evaluation::new(&mut agent, Box::new(CartPole::default)).next().unwrap();
+        Evaluation::new(&mut agent, Box::new(MountainCar::default)).next().unwrap();
 
 
     println!("Solution \u{21D2} {} steps | reward {}",
