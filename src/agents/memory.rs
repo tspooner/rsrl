ffb818ea1d5fd7c0067725837dbba1394f322af4
Tom Spooner <spooner10000@gmail.com>
2017-06-07 15:33:47 +0100

    Add tests for traces

--- src/agents/memory.rs
+++ src/agents/memory.rs
@@ -19,10 +19,7 @@ impl Trace {
     pub fn get(&self) -> &Array1<f64> {
         match self {
             &Trace::Accumulating { ref eligibility, .. } |
-                &Trace::Replacing { ref eligibility, .. } =>
-            {
-                eligibility
-            },
+                &Trace::Replacing { ref eligibility, .. } => eligibility,
         }
     }
 
@@ -38,14 +35,66 @@ impl Trace {
 
     pub fn update(&mut self, phi: &Array1<f64>) {
         match self {
-            &mut Trace::Accumulating { ref mut eligibility, lambda } => {
+            &mut Trace::Accumulating { ref mut eligibility, lambda } =>
+            {
                 eligibility.add_assign(phi);
-
             },
-            &mut Trace::Replacing { ref mut eligibility, lambda } => {
+            &mut Trace::Replacing { ref mut eligibility, lambda } =>
+            {
                 eligibility.add_assign(phi);
                 eligibility.map_inplace(|val| *val = val.min(1.0));
             },
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::Trace;
+    use ndarray::{ArrayBase, arr1};
+
+    #[test]
+    fn test_accumulating() {
+        let mut trace = Trace::Accumulating {
+            lambda: 0.95.into(),
+            eligibility: arr1(&[0.0f64; 10])
+        };
+
+        assert_eq!(trace.get(), &arr1(&[0.0f64; 10]));
+
+        trace.decay(1.0);
+        assert_eq!(trace.get(), &arr1(&[0.0f64; 10]));
+
+        trace.update(&arr1(&[1.0f64; 10]));
+        assert_eq!(trace.get(), &arr1(&[1.0f64; 10]));
+
+        trace.decay(1.0);
+        assert_eq!(trace.get(), &arr1(&[0.95f64; 10]));
+
+        trace.update(&arr1(&[1.0f64; 10]));
+        assert_eq!(trace.get(), &arr1(&[1.95f64; 10]));
+    }
+
+    #[test]
+    fn test_replacing() {
+        let mut trace = Trace::Replacing {
+            lambda: 0.95.into(),
+            eligibility: arr1(&[0.0f64; 10])
+        };
+
+        assert_eq!(trace.get(), &arr1(&[0.0f64; 10]));
+
+        trace.decay(1.0);
+        assert_eq!(trace.get(), &arr1(&[0.0f64; 10]));
+
+        trace.update(&arr1(&[1.0f64; 10]));
+        assert_eq!(trace.get(), &arr1(&[1.0f64; 10]));
+
+        trace.decay(1.0);
+        assert_eq!(trace.get(), &arr1(&[0.95f64; 10]));
+
+        trace.update(&arr1(&[1.0f64; 10]));
+        assert_eq!(trace.get(), &arr1(&[1.0f64; 10]));
     }
 }
