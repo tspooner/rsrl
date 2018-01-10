7fda420e80994aa36d07d5744b103ca80f20ae41
Tom Spooner <spooner10000@gmail.com>
2018-01-10 11:09:19 +0000

    docs(policies): add docs for `Policy` trait

--- src/policies/mod.rs
+++ src/policies/mod.rs
@@ -1,10 +1,15 @@
 //! Agent policy module.
 
 // TODO: Add support for generic action spaces representation.
+/// Policy trait for functions that select between a set of values.
 pub trait Policy {
+    /// Sample the policy distribution for a set of exogenous input values.
     fn sample(&mut self, qs: &[f64]) -> usize;
+
+    /// Return the probability of selecting each value in a given set of input values.
     fn probabilities(&mut self, qs: &[f64]) -> Vec<f64>;
 
+    /// Update the policy after reaching a terminal state.
     fn handle_terminal(&mut self) {}
 }
 
