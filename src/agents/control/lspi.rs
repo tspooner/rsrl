5dd0f3088eac913f54af9a21b13fd4f6f41659d6
Tom Spooner <spooner10000@gmail.com>
2018-02-08 14:04:33 +0000

    refactor(agents): change the way samples are stored in lstd methods

--- src/agents/control/lspi.rs
+++ src/agents/control/lspi.rs
@@ -17,10 +17,9 @@ pub struct LSPILambda<S: Space, M: Projector<S>, P: Policy> {
     a: Array2<f64>,
     b: Array1<f64>,
 
-    last_state: Option<S::Repr>,
-    trajectory: Vec<(S::Repr, f64)>,
+    trajectory: Vec<(S::Repr, f64, S::Repr)>,
 
-    pub fa: Linear<S, M>,
+    pub fa_w: Linear<S, M>,
     pub policy: P,
 
     pub gamma: Parameter,
@@ -30,11 +29,11 @@ pub struct LSPILambda<S: Space, M: Projector<S>, P: Policy> {
 }
 
 impl<S: Space, M: Projector<S>, P: Policy> LSPILambda<S, M, P> {
-    pub fn new<T1, T2>(trace: Trace, fa: Linear<S, M>, policy: P, gamma: T1, lambda: T2) -> Self
+    pub fn new<T1, T2>(trace: Trace, fa_w: Linear<S, M>, policy: P, gamma: T1, lambda: T2) -> Self
         where T1: Into<Parameter>,
               T2: Into<Parameter>,
     {
-        let n_features = fa.projector.size();
+        let n_features = fa_w.projector.size();
 
         LSPILambda {
             trace: trace,
@@ -42,10 +41,9 @@ impl<S: Space, M: Projector<S>, P: Policy> LSPILambda<S, M, P> {
             a: Array2::zeros((n_features, n_features)),
             b: Array1::zeros((n_features,)),
 
-            last_state: None,
             trajectory: vec![],
 
-            fa: fa,
+            fa_w: fa_w,
             policy: policy,
 
             gamma: gamma.into(),
@@ -60,8 +58,9 @@ impl<S: Space, M: Projector<S>, P: Policy> Agent<S> for LSPILambda<S, M, P> {
     type Sample = Transition<S, ActionSpace>;
 
     fn handle_sample(&mut self, sample: &Self::Sample) {
-        self.last_state = Some(sample.to.state().clone());
-        self.trajectory.push((sample.from.state().clone(), sample.reward));
+        let entry = (sample.from.state().clone(), sample.reward, sample.to.state().clone());
+
+        self.trajectory.push(entry);
     }
 
     fn handle_terminal(&mut self, _: &S::Repr) {
@@ -74,40 +73,35 @@ impl<S: Space, M: Projector<S>, P: Policy> Agent<S> for LSPILambda<S, M, P> {
 
 impl<S: Space, M: Projector<S>, P: Policy> BatchAgent<S> for LSPILambda<S, M, P> {
     fn consolidate(&mut self) {
-        while let Some(ref ns) = self.last_state {
-            if let Some((s, r)) = self.trajectory.pop() {
-                let nqs: Vec<f64> = self.fa.evaluate(ns);
-                let exp_nqs = dot(&nqs, &Greedy.probabilities(nqs.as_slice()));
+        for (s, r, ns) in self.trajectory.drain(0..) {
+            let phi_s = self.fa_w.projector.project_expanded(&s);
 
-                let phi_s = self.fa.projector.project_expanded(&s);
+            self.trace.decay(self.gamma*self.lambda);
+            self.trace.update(&phi_s);
 
-                self.trace.decay(self.gamma*self.lambda);
-                self.trace.update(&phi_s);
+            let z = self.trace.get();
+            let z = z.view().into_shape((z.len(), 1)).unwrap();
 
-                let z = self.trace.get();
-                let z = z.view().into_shape((z.len(), 1)).unwrap();
+            let nqs: Vec<f64> = self.fa_w.evaluate(&ns);
+            let exp_nqs = dot(&nqs, &Greedy.probabilities(nqs.as_slice()));
 
-                let error_vec = phi_s.clone() - self.gamma*exp_nqs;
-                let update_mat = z.dot(&error_vec.view().into_shape((1, phi_s.len())).unwrap());
+            let error_vec = phi_s.clone() - self.gamma*exp_nqs;
+            let update_mat = z.dot(&error_vec.view().into_shape((1, phi_s.len())).unwrap());
 
-                self.a.zip_mut_with(&update_mat, move |y, &x| *y += x);
-                self.b.scaled_add(r, &phi_s);
-            } else {
-                break;
-            }
+            self.a.zip_mut_with(&update_mat, move |y, &x| *y += x);
+            self.b.scaled_add(r, &phi_s);
         }
 
-        let d = (self.b.dim(), 1);
-        self.fa.assign(self.a.solve_into(self.b.clone()).unwrap().into_shape(d).unwrap());
+        self.fa_w.assign(self.a.solve(&self.b).unwrap().into_shape((self.b.dim(), 1)).unwrap());
 
-        self.last_state = None;
-        self.trajectory.clear();
+        self.a.fill(0.0);
+        self.b.fill(0.0);
     }
 }
 
 impl<S: Space, M: Projector<S>, P: Policy> LinearAgent<S> for LSPILambda<S, M, P> {
     fn weights(&self) -> Array2<f64> {
-        self.fa.weights.clone()
+        self.fa_w.weights.clone()
     }
 }
 
@@ -117,13 +111,13 @@ impl<S: Space, M: Projector<S>, P: Policy> Controller<S, ActionSpace> for LSPILa
     }
 
     fn mu(&mut self, s: &S::Repr) -> usize {
-        let qs: Vec<f64> = self.fa.evaluate(s);
+        let qs: Vec<f64> = self.fa_w.evaluate(s);
 
         self.policy.sample(qs.as_slice())
     }
 
     fn evaluate_policy<T: Policy>(&self, p: &mut T, s: &S::Repr) -> usize {
-        let qs: Vec<f64> = self.fa.evaluate(s);
+        let qs: Vec<f64> = self.fa_w.evaluate(s);
 
         p.sample(qs.as_slice())
     }
--- src/agents/prediction/lspe.rs
+++ src/agents/prediction/lspe.rs
@@ -11,8 +11,7 @@ pub struct LambdaLSPE<S: Space, P: Projector<S>> {
     a: Array2<f64>,
     b: Array1<f64>,
 
-    last_state: Option<S::Repr>,
-    trajectory: Vec<(S::Repr, f64)>,
+    trajectory: Vec<(S::Repr, S::Repr, f64)>,
 
     pub fa: Linear<S, P>,
     pub alpha: Parameter,
@@ -34,7 +33,6 @@ impl<S: Space, P: Projector<S>> LambdaLSPE<S, P> {
             a: Array2::zeros((n_features, n_features)),
             b: Array1::zeros((n_features,)),
 
-            last_state: None,
             trajectory: vec![],
 
             fa: fa,
@@ -51,8 +49,7 @@ impl<S: Space, P: Projector<S>> Agent<S> for LambdaLSPE<S, P> {
     type Sample = (S::Repr, S::Repr, f64);
 
     fn handle_sample(&mut self, sample: &Self::Sample) {
-        self.last_state = Some(sample.1.clone());
-        self.trajectory.push((sample.0.clone(), sample.2));
+        self.trajectory.push(sample.clone());
     }
 
     fn handle_terminal(&mut self, _: &S::Repr) {
@@ -68,30 +65,24 @@ impl<S: Space, P: Projector<S>> BatchAgent<S> for LambdaLSPE<S, P> {
     fn consolidate(&mut self) {
         let mut error = 0.0;
 
-        while let Some(ref ns) = self.last_state {
-            if let Some((s, r)) = self.trajectory.pop() {
-                let phi_s = self.fa.projector.project(&s);
+        while let Some((s, ns, r)) = self.trajectory.pop() {
+            let phi_s = self.fa.projector.project(&s);
 
-                let v: f64 = self.fa.evaluate_phi(&phi_s);
-                let nv: f64 = self.fa.evaluate(ns);
+            let v: f64 = self.fa.evaluate_phi(&phi_s);
+            let nv: f64 = self.fa.evaluate(&ns);
 
-                error = self.gamma*self.lambda*error + (r + self.gamma*nv - v);
+            error = self.gamma*self.lambda*error + (r + self.gamma*nv - v);
 
-                let phi_s = self.fa.projector.expand_projection(phi_s);
+            let phi_s = self.fa.projector.expand_projection(phi_s);
 
-                self.b.scaled_add(v + error, &phi_s);
-                self.a += phi_s.dot(&phi_s);
-            } else {
-                break;
-            }
+            self.b.scaled_add(v + error, &phi_s);
+            self.a += phi_s.dot(&phi_s);
         }
 
         let estimate = self.a.solve_into(self.b.clone()).unwrap();
         let update = estimate - self.fa.weights.column(0);
 
         self.fa.update_phi(&Projection::Dense(update), self.alpha.value());
-
-        self.last_state = None;
         self.trajectory.clear();
     }
 }
