43362113dee771a613b7e3337b7cf5584dd58381
Tom Spooner <spooner10000@gmail.com>
2018-05-16 10:05:08 +0100

    feat(domains): implement Into methods for Observation and Transition

--- src/domains/mod.rs
+++ src/domains/mod.rs
@@ -5,7 +5,59 @@ use geometry::dimensions;
 use geometry::dimensions::Dimension;
 use std::collections::HashSet;
 
+macro_rules! impl_into {
+    (Observation<S, $type:ty> => Observation<S, ()>) => (
+        impl<S> Into<Observation<S, ()>> for Observation<S, $type> {
+            fn into(self) -> Observation<S, ()> {
+                use self::Observation::*;
+
+                match self {
+                    Full { state, .. } => Full { state: state, actions: HashSet::new() },
+                    Partial { state, .. } => Partial { state: state, actions: HashSet::new() },
+                    Terminal(state) => Terminal(state),
+                }
+            }
+        }
+
+        impl<'a, S: Clone> From<&'a Observation<S, $type>> for Observation<S, ()> {
+            fn from(obs: &'a Observation<S, $type>) -> Observation<S, ()> {
+                use self::Observation::*;
+
+                match obs {
+                    Full { state, .. } => Full { state: state.clone(), actions: HashSet::new() },
+                    Partial { state, .. } => Partial { state: state.clone(), actions: HashSet::new() },
+                    Terminal(state) => Terminal(state.clone()),
+                }
+            }
+        }
+    );
+    (Transition<S, $type:ty> => Transition<S, ()>) => (
+        impl<S> Into<Transition<S, ()>> for Transition<S, $type> {
+            fn into(self) -> Transition<S, ()> {
+                Transition {
+                    from: self.from.into(),
+                    action: (),
+                    reward: self.reward,
+                    to: self.to.into(),
+                }
+            }
+        }
+
+        impl<'a, S: Clone> From<&'a Transition<S, $type>> for Transition<S, ()> {
+            fn from(t: &'a Transition<S, $type>) -> Transition<S, ()> {
+                Transition {
+                    from: (&t.from).into(),
+                    action: (),
+                    reward: t.reward,
+                    to: (&t.to).into(),
+                }
+            }
+        }
+    )
+}
+
 /// Container class for data associated with a domain observation.
+#[derive(Clone)]
 pub enum Observation<S, A> {
     /// Fully observed state of the environment.
     Full {
@@ -41,12 +93,26 @@ impl<S, A> Observation<S, A> {
     }
 }
 
+impl_into!(Observation<S, u8> => Observation<S, ()>);
+impl_into!(Observation<S, u16> => Observation<S, ()>);
+impl_into!(Observation<S, u32> => Observation<S, ()>);
+impl_into!(Observation<S, u64> => Observation<S, ()>);
+impl_into!(Observation<S, usize> => Observation<S, ()>);
+impl_into!(Observation<S, i8> => Observation<S, ()>);
+impl_into!(Observation<S, i16> => Observation<S, ()>);
+impl_into!(Observation<S, i32> => Observation<S, ()>);
+impl_into!(Observation<S, i64> => Observation<S, ()>);
+impl_into!(Observation<S, isize> => Observation<S, ()>);
+impl_into!(Observation<S, f32> => Observation<S, ()>);
+impl_into!(Observation<S, f64> => Observation<S, ()>);
+
 /// Container class for data associated with a domain transition.
+#[derive(Clone)]
 pub struct Transition<S, A> {
     /// State transitioned _from_, `s`.
     pub from: Observation<S, A>,
 
-    /// Action taken to initiate the transition.
+    /// Action taken to initiate the transition (control tasks).
     pub action: A,
 
     /// Reward obtained from the transition.
@@ -55,6 +121,19 @@ pub struct Transition<S, A> {
     /// State transitioned _to_, `s'`.
     pub to: Observation<S, A>,
 }
+
+impl_into!(Transition<S, u8> => Transition<S, ()>);
+impl_into!(Transition<S, u16> => Transition<S, ()>);
+impl_into!(Transition<S, u32> => Transition<S, ()>);
+impl_into!(Transition<S, u64> => Transition<S, ()>);
+impl_into!(Transition<S, usize> => Transition<S, ()>);
+impl_into!(Transition<S, i8> => Transition<S, ()>);
+impl_into!(Transition<S, i16> => Transition<S, ()>);
+impl_into!(Transition<S, i32> => Transition<S, ()>);
+impl_into!(Transition<S, i64> => Transition<S, ()>);
+impl_into!(Transition<S, isize> => Transition<S, ()>);
+impl_into!(Transition<S, f32> => Transition<S, ()>);
+impl_into!(Transition<S, f64> => Transition<S, ()>);
 
 /// An interface for constructing reinforcement learning problem domains.
 pub trait Domain {
