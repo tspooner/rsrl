c8bc48fb7f30a26734edc9b0dba7d9d38bc78be6
Tom Spooner <spooner10000@gmail.com>
2018-06-26 17:57:15 +0100

    test(utils): remove doctests for private functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,4 @@
 #![allow(dead_code)]
-
 use std::f64;
 
 pub fn argmaxima(vals: &[f64]) -> (f64, Vec<usize>) {
@@ -30,29 +29,29 @@ pub fn sub2ind(dims: &[usize], inds: &[usize]) -> usize {
 /// Given a vector containing a partial Cartesian product, and a list of items,
 /// return a vector adding the list of items to the partial Cartesian product.
 ///
-/// # Example
-///
-/// ```
-/// use rsrl::utils::partial_cartesian;
-///
-/// let partial_product = vec![vec![1, 4], vec![1, 5], vec![2, 4], vec![2, 5]];
-/// let items = vec![6, 7];
-/// let next_product = partial_cartesian(partial_product, &items);
-///
-/// assert_eq!(
-///     next_product,
-///     vec![
-///         vec![1, 4, 6],
-///         vec![1, 4, 7],
-///         vec![1, 5, 6],
-///         vec![1, 5, 7],
-///         vec![2, 4, 6],
-///         vec![2, 4, 7],
-///         vec![2, 5, 6],
-///         vec![2, 5, 7],
-///     ]
-/// );
-/// ```
+// /// # Example
+// ///
+// /// ```
+// /// use rsrl::utils::partial_cartesian;
+// ///
+// /// let partial_product = vec![vec![1, 4], vec![1, 5], vec![2, 4], vec![2, 5]];
+// /// let items = vec![6, 7];
+// /// let next_product = partial_cartesian(partial_product, &items);
+// ///
+// /// assert_eq!(
+// ///     next_product,
+// ///     vec![
+// ///         vec![1, 4, 6],
+// ///         vec![1, 4, 7],
+// ///         vec![1, 5, 6],
+// ///         vec![1, 5, 7],
+// ///         vec![2, 4, 6],
+// ///         vec![2, 4, 7],
+// ///         vec![2, 5, 6],
+// ///         vec![2, 5, 7],
+// ///     ]
+// /// );
+// /// ```
 ///
 /// Pulled from [here](https://gist.github.com/kylewlacy/115965b40e02a3325558).
 pub fn partial_cartesian<T: Clone>(a: Vec<Vec<T>>, b: &Vec<T>) -> Vec<Vec<T>> {
@@ -72,28 +71,28 @@ pub fn partial_cartesian<T: Clone>(a: Vec<Vec<T>>, b: &Vec<T>) -> Vec<Vec<T>> {
 
 /// Computes the Cartesian product of lists[0] * lists[1] * ... * lists[n].
 ///
-/// # Example
-///
-/// ```
-/// use rsrl::utils::cartesian_product;
-///
-/// let lists = vec![vec![1, 2], vec![4, 5], vec![6, 7]];
-/// let product = cartesian_product(&lists);
-///
-/// assert_eq!(
-///     product,
-///     vec![
-///         vec![1, 4, 6],
-///         vec![1, 4, 7],
-///         vec![1, 5, 6],
-///         vec![1, 5, 7],
-///         vec![2, 4, 6],
-///         vec![2, 4, 7],
-///         vec![2, 5, 6],
-///         vec![2, 5, 7],
-///     ]
-/// );
-/// ```
+// /// # Example
+// ///
+// /// ```
+// /// use rsrl::utils::cartesian_product;
+// ///
+// /// let lists = vec![vec![1, 2], vec![4, 5], vec![6, 7]];
+// /// let product = cartesian_product(&lists);
+// ///
+// /// assert_eq!(
+// ///     product,
+// ///     vec![
+// ///         vec![1, 4, 6],
+// ///         vec![1, 4, 7],
+// ///         vec![1, 5, 6],
+// ///         vec![1, 5, 7],
+// ///         vec![2, 4, 6],
+// ///         vec![2, 4, 7],
+// ///         vec![2, 5, 6],
+// ///         vec![2, 5, 7],
+// ///     ]
+// /// );
+// /// ```
 ///
 /// Pulled from [here](https://gist.github.com/kylewlacy/115965b40e02a3325558).
 pub fn cartesian_product<T: Clone>(lists: &Vec<Vec<T>>) -> Vec<Vec<T>> {
