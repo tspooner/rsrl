39d21a3ab4a49aca73ea42e26cabaeead925b316
Tom Spooner <spooner10000@gmail.com>
2018-11-27 15:32:22 +0000

    feat(core): add BatchAlgorithm trait

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -6,6 +6,23 @@ pub trait Algorithm<S, A> {
     fn handle_terminal(&mut self, sample: &Transition<S, A>);
 }
 
+pub trait BatchAlgorithm<S, A>: Algorithm<S, A> {
+    fn solve(&mut self);
+
+    fn handle_batch(&mut self, samples: &Vec<Transition<S, A>>) {
+        use domains::Observation;
+
+        samples.into_iter().rev().for_each(|s| {
+            match s.to {
+                Observation::Terminal(_) => self.handle_terminal(s),
+                _ => self.handle_sample(s),
+            }
+        });
+
+        self.solve();
+    }
+}
+
 pub trait Controller<S, A>: Algorithm<S, A> {
     /// Sample the target policy for a given state `s`.
     fn sample_target(&mut self, s: &S) -> A;
@@ -17,7 +34,7 @@ pub trait Controller<S, A>: Algorithm<S, A> {
 pub trait Predictor<S, A>: Algorithm<S, A> {
     fn predict_v(&mut self, s: &S) -> f64 { unimplemented!() }
     fn predict_qs(&mut self, s: &S) -> Vector<f64> { unimplemented!() }
-    fn predict_qsa(&mut self, s: &S, a: A) -> f64 { unimplemented!() }
+    fn predict_qsa(&mut self, s: &S, a: A) -> f64 { self.predict_v(s) }
 }
 
 mod memory;
