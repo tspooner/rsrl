ede421529c1fa2aa2498a66ff3374c699b95f864
Tom Spooner <spooner10000@gmail.com>
2019-02-14 13:11:33 +0000

    feat(control): add configurable behaviour policy to CACLA

--- src/control/actor_critic/cacla.rs
+++ src/control/actor_critic/cacla.rs
@@ -4,23 +4,32 @@ use crate::policies::{Policy, ParameterisedPolicy};
 use std::marker::PhantomData;
 
 /// Continuous Actor-Critic Learning Automaton
-pub struct CACLA<C, P> {
+pub struct CACLA<C, PT, PB> {
     pub critic: Shared<C>,
-    pub policy: Shared<P>,
+
+    pub target_policy: Shared<PT>,
+    pub behaviour_policy: Shared<PB>,
 
     pub alpha: Parameter,
     pub gamma: Parameter,
 }
 
-impl<C, P> CACLA<C, P> {
-    pub fn new<T1, T2>(critic: Shared<C>, policy: Shared<P>, alpha: T1, gamma: T2) -> Self
+impl<C, PT, PB> CACLA<C, PT, PB> {
+    pub fn new<T1, T2>(
+        critic: Shared<C>,
+        target_policy: Shared<PT>,
+        behaviour_policy: Shared<PB>,
+        alpha: T1, gamma: T2
+    ) -> Self
     where
         T1: Into<Parameter>,
         T2: Into<Parameter>,
     {
         CACLA {
             critic,
-            policy,
+
+            target_policy,
+            behaviour_policy,
 
             alpha: alpha.into(),
             gamma: gamma.into(),
@@ -28,28 +37,32 @@ impl<C, P> CACLA<C, P> {
     }
 }
 
-impl<C, P> Algorithm for CACLA<C, P>
+impl<C, PT, PB> Algorithm for CACLA<C, PT, PB>
 where
     C: Algorithm,
-    P: Algorithm,
+    PT: Algorithm,
+    PB: Algorithm,
 {
     fn handle_terminal(&mut self) {
         self.alpha = self.alpha.step();
         self.gamma = self.gamma.step();
 
         self.critic.borrow_mut().handle_terminal();
-        self.policy.borrow_mut().handle_terminal();
+
+        self.target_policy.borrow_mut().handle_terminal();
+        self.behaviour_policy.borrow_mut().handle_terminal();
     }
 }
 
-impl<S, C, P> OnlineLearner<S, P::Action> for CACLA<C, P>
+impl<S, C, PT, PB> OnlineLearner<S, PT::Action> for CACLA<C, PT, PB>
 where
-    C: OnlineLearner<S, P::Action> + ValuePredictor<S>,
-    P: ParameterisedPolicy<S, Action = f64>,
+    C: OnlineLearner<S, PT::Action> + ValuePredictor<S>,
+    PT: ParameterisedPolicy<S, Action = f64>,
+    PB: Algorithm,
 {
-    fn handle_transition(&mut self, t: &Transition<S, P::Action>) {
+    fn handle_transition(&mut self, t: &Transition<S, PT::Action>) {
         let mut critic = self.critic.borrow_mut();
-        let mut policy = self.policy.borrow_mut();
+        let mut policy = self.target_policy.borrow_mut();
 
         let s = t.from.state();
         let v = critic.predict_v(s);
@@ -69,7 +82,7 @@ where
     }
 }
 
-impl<S, C, P> ValuePredictor<S> for CACLA<C, P>
+impl<S, C, PT, PB> ValuePredictor<S> for CACLA<C, PT, PB>
 where
     C: ValuePredictor<S>,
 {
@@ -78,35 +91,30 @@ where
     }
 }
 
-impl<S, C, P> ActionValuePredictor<S, P::Action> for CACLA<C, P>
+impl<S, C, PT, PB> ActionValuePredictor<S, PT::Action> for CACLA<C, PT, PB>
 where
-    C: ActionValuePredictor<S, P::Action>,
-    P: Policy<S>,
+    C: ActionValuePredictor<S, PT::Action>,
+    PT: Policy<S>,
 {
     fn predict_qs(&mut self, s: &S) -> Vector<f64> {
         self.critic.borrow_mut().predict_qs(s)
     }
 
-    fn predict_qsa(&mut self, s: &S, a: P::Action) -> f64 {
+    fn predict_qsa(&mut self, s: &S, a: PT::Action) -> f64 {
         self.critic.borrow_mut().predict_qsa(s, a)
     }
 }
 
-use rand::{
-    distributions::{Distribution, Normal as NormalDist},
-    rngs::ThreadRng,
-    thread_rng,
-};
-
-impl<S, C, P> Controller<S, P::Action> for CACLA<C, P>
+impl<S, C, PT, PB> Controller<S, PT::Action> for CACLA<C, PT, PB>
 where
-    P: ParameterisedPolicy<S, Action = f64>,
+    PT: ParameterisedPolicy<S>,
+    PB: Policy<S, Action = PT::Action>,
 {
-    fn sample_target(&mut self, s: &S) -> P::Action {
-        self.policy.borrow_mut().sample(s)
+    fn sample_target(&mut self, s: &S) -> PT::Action {
+        self.target_policy.borrow_mut().sample(s)
     }
 
-    fn sample_behaviour(&mut self, s: &S) -> P::Action {
-        NormalDist::new(self.policy.borrow_mut().sample(s), 5.0).sample(&mut thread_rng())
+    fn sample_behaviour(&mut self, s: &S) -> PB::Action {
+        self.behaviour_policy.borrow_mut().sample(s)
     }
 }
