1c71fdcf2aa79ea7fd787ca38238883089484d14
Tom Spooner <spooner10000@gmail.com>
2019-04-08 15:23:22 +0100

    docs(examples): add QLearning example script

---
+++ examples/q_learning.rs
@@ -0,0 +1,48 @@
+extern crate rsrl;
+#[macro_use]
+extern crate slog;
+
+use rsrl::{
+    control::td::QLearning,
+    core::{make_shared, run, Evaluation, Parameter, SerialExperiment},
+    domains::{Domain, MountainCar},
+    fa::{basis::{Composable, fixed::Fourier}, LFA},
+    geometry::Space,
+    logging,
+    policies::fixed::{EpsilonGreedy, Greedy, Random},
+};
+
+fn main() {
+    let domain = MountainCar::default();
+    let mut agent = {
+        let n_actions = domain.action_space().card().into();
+
+        let bases = Fourier::from_space(3, domain.state_space()).with_constant();
+        let q_func = make_shared(LFA::vector(bases, n_actions));
+
+        let policy = EpsilonGreedy::new(
+            Greedy::new(q_func.clone()),
+            Random::new(n_actions),
+            Parameter::exponential(0.3, 0.001, 0.99),
+        );
+
+        QLearning::new(q_func, policy, 0.001, 0.99)
+    };
+
+    let logger = logging::root(logging::stdout());
+    let domain_builder = Box::new(MountainCar::default);
+
+    // Training phase:
+    let _training_result = {
+        // Start a serial learning experiment up to 1000 steps per episode.
+        let e = SerialExperiment::new(&mut agent, domain_builder.clone(), 1000);
+
+        // Realise 1000 episodes of the experiment generator.
+        run(e, 1000, Some(logger.clone()))
+    };
+
+    // Testing phase:
+    let testing_result = Evaluation::new(&mut agent, domain_builder).next().unwrap();
+
+    info!(logger, "solution"; testing_result);
+}
